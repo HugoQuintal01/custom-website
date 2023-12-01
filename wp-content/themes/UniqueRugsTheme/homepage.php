0cc4786c0ad99e2490fbd086fc387d20010c2541
HugoQuintal01 <102548094+HugoQuintal01@users.noreply.github.com>
2023-12-01 17:08:21 +0000

    update

--- wp-content/themes/UniqueRugsTheme/homepage.php
+++ wp-content/themes/UniqueRugsTheme/homepage.php
@@ -22,7 +22,17 @@ Template Name: Custom Homepage
     </section>
     <section class="gridrowfull work-with-us-homepage">
         <div class="work-with-us-homepage-image col-ld-3 col-d-3 col-t-12 col-12">
-            
+            <img src="http://localhost/uniquerugs/wp-content/uploads/2023/12/work-with-us.jpg" alt="work-with-us">
+        </div>
+        <div class="work-with-us-homepage-text col-ld-6 col-d-6 col-t-12 col-12">
+            <p class="col-ld-10 col-d-10 col-t-12 col-12">“With over 77 years of experience and as Europe’s leading manufacturer, we are committed to providing our customers with an outstanding service. Whether you are looking for a bespoke or individualised solution, Unique Rugs is committed to guiding you throughout the process to meet your needs. With the use of the most advanced technologies and expert craftmanship we guarantee the highest quality and precision.”</p>
+            <p class="col-ld-10 col-d-10 col-t-12 col-12 author">Calixto Valenti</p>
+            <p class="col-ld-10 col-d-10 col-t-12 col-12 author-position">CEO</p>
+        </div>
+    </section>
+    <section class="gridrowfull unique-rugs-homepage">
+        <div class="col-ld-3 col-d-3 col-t-12 col-12">
+            <img src="http://localhost/uniquerugs/wp-content/uploads/2023/12/unique-rugs-01.jpg" alt="unique-rugs-01">
         </div>
     </section>
 
