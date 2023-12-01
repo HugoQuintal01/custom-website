9014a5505538842a39b4eab7d7290e9602beab75
HugoQuintal01 <102548094+HugoQuintal01@users.noreply.github.com>
2023-12-01 16:20:42 +0000

    update

--- wp-content/themes/UniqueRugsTheme/homepage.php
+++ wp-content/themes/UniqueRugsTheme/homepage.php
@@ -15,9 +15,14 @@ Template Name: Custom Homepage
 <body <?php body_class(); ?>> <!-- Add this line to include body classes -->
     
     <section class="gridrowfull banner-homepage">
-        <div class="gridrowfull titles-banner-homepage col-12 col-d-7 col-t-12">
-            <h1 class="col-12 col-d-8 col-t-12">Step into a world full of creativity</h1>
-            <span class="col-12 col-d-12 col-t-12">We roll out the tailored rugs to your specific needs!</span>
+        <div class="gridrowfull titles-banner-homepage col-12 col-ld-7 col-d-10 col-t-12">
+            <h1 class="col-12 col-t-12 col-d-7 col-ld-8">Step into a world full of creativity</h1>
+            <span class="col-12 col-ld-12 col-d-12 col-t-12">We roll out the tailored rugs to your specific needs!</span>
+        </div>
+    </section>
+    <section class="gridrowfull work-with-us-homepage">
+        <div class="work-with-us-homepage-image col-ld-3 col-d-3 col-t-12 col-12">
+            
         </div>
     </section>
 
