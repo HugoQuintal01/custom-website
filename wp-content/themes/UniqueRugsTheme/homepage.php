05eeb2d2ae663fa38ed4d81ec430bc5aaade9a75
HugoQuintal01 <102548094+HugoQuintal01@users.noreply.github.com>
2023-12-01 11:33:02 +0000

    update

---
+++ wp-content/themes/UniqueRugsTheme/homepage.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Custom Homepage
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?> <!-- Add this line to include WordPress head elements -->
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style/styles.css">
+    <title>Homepage</title>
+</head>
+<body <?php body_class(); ?>> <!-- Add this line to include body classes -->
+    <h1 class="teste123">Teste</h1>
+
+    <?php wp_footer(); ?> <!-- Add this line to include WordPress footer elements -->
+</body>
+</html>
