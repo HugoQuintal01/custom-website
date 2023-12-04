11c6366fcf659a67a888dee82be8b5e1259af50b
HugoQuintal01 <102548094+HugoQuintal01@users.noreply.github.com>
2023-12-04 11:20:44 +0000

    update

--- wp-content/themes/UniqueRugsTheme/header.php
+++ wp-content/themes/UniqueRugsTheme/header.php
@@ -8,7 +8,7 @@
 <body <?php body_class(); ?>>
 <section class="menu gridrowfull col-ld-12 col-d-12 col-t-12 col-12">
     <div class="logo-website col-ld-5 col-d-3 col-t-6 col-6">
-        <a href="/">
+        <a href="#">
             <img src="http://localhost/uniquerugs/wp-content/uploads/2023/12/unique-rugs-logo-white.svg" alt="unique-rugs-logo-white">
         </a>
     </div>
